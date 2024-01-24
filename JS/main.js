c1002bec2ebd0c18b3894c49e8b485d23c3fc0de
savellanoaj <savellanoaj@gmail.com>
2024-01-24 09:16:22 -0800

    Added Previous Files

---
+++ JS/main.js
@@ -0,0 +1,5 @@
+// console.log("AJ, Isaac, Mallory")
+
+//set current year in span with id of this-year
+// let d = new Date(); let thisYear = d.getFullYear();
+// document.getElementById("this-year").innerHTML = thisYear;
